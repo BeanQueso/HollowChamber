8ad7e0beaee623014ce039eb156764a05eaf8111
George <114946309+GeorgeVargheseG@users.noreply.github.com>
2024-05-24 12:28:09 -0400

    My First commit
    xoxo gossip girl

--- components/Revolver.py
+++ components/Revolver.py
@@ -1,39 +1,28 @@
 import random
 
-class Revolver():
-    
-    def __init__(self,maxCapacity):
-        self.previousIndices = [False] * maxCapacity
-        self.revolverIndex = random.randint(1,maxCapacity) - 1;
-        self.myIndex = random.randint(1,maxCapacity) - 1;
-        self.maxCapacity = maxCapacity
-    
-    def spinRevolver(self,spins):
-        for i in range(spins):
-            self.myIndex = random.randint(1,self.maxCapacity) - 1;
 
-    def canShoot(self,spins):
+class Revolver():
+
+    def __init__(self, maxCapacity):
+        self.previousIndices = [False] * maxCapacity
+        self.revolverIndex = random.randint(1, maxCapacity) - 1
+        self.myIndex = random.randint(1, maxCapacity) - 1
+        self.maxCapacity = maxCapacity
+
+    def spinRevolver(self, spins):
+        for i in range(spins):
+            self.myIndex = random.randint(1, self.maxCapacity) - 1
+
+    def canShoot(self, spins):
         self.spinRevolver(spins)
-        if(self.myIndex == self.revolverIndex):
+        if (self.myIndex == self.revolverIndex):
             return True
         return False
 
     def getChances(self):
         for i in self.previousIndices:
-            if(i):
+            if (i):
+                return i
 
-
-    
-
-
-
-    
-
-
-
-        
-
-            
-                
-            
-            
+    def getSomething():
+        pass
